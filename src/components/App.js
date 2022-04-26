d63313f4252717982822dd0717b9c80922284038
MiriamPoveda <mpoveda94@gmail.com>
2022-04-26 09:07:16 +0200

    Paint phrases data

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import '../styles/App.scss';
 import friendsPhrases from '../data/friends.json';
+import {useState} from 'react';
 
 function App() {
+
+  // Variables de estado 
+
+  /* Lista de frases */
+  const [data, setData] = useState(friendsPhrases);
+
+  // Funciones
+
+  /* Pintar los datos de la lista facilitada */
+  const paintData = data.map((phrases, index) =>
+    <li key={index}>
+      <p>{phrases.quote}</p>
+      <p>{phrases.character}</p>
+    </li>
+  );
+
+  // HTML 
+
   return (
     <div>
       <header>
         <h1>Frases de Friends</h1>
       </header>
       <main>
-        <ul></ul>
+        <ul>
+          {paintData}
+        </ul>
         <div>
           <h2>Añadir una nueva frase</h2>
         <form>
