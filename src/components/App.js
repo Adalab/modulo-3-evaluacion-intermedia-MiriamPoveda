4d689bcf61dccb5db421c777d9bea8bfc1bd5e02
MiriamPoveda <mpoveda94@gmail.com>
2022-04-26 08:59:38 +0200

    Add initial structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import '../styles/App.scss';
+import friendsPhrases from '../data/friends.json';
 
 function App() {
   return (
     <div>
-      
+      <header>
+        <h1>Frases de Friends</h1>
+      </header>
+      <main>
+        <ul></ul>
+        <div>
+          <h2>Añadir una nueva frase</h2>
+        <form>
+          <label htmlFor=""></label>
+          <input type="text" />
+          <label htmlFor=""></label>
+          <input type="text" />
+          <input type="submit" value="Añadir una nueva frase"/>
+        </form>
+        </div>
+      </main>
     </div>
   );
 }
